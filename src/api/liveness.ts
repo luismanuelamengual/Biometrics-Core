bbcca23ae1679bc08c6e05d7559118d4bdff7be7
luis.amengual <luis.amengual@globant.com>
2021-10-26 14:19:47 -0300

    Correccion en la api de liveness

--- src/api/liveness.ts
+++ src/api/liveness.ts
@@ -2,12 +2,12 @@ import {Api} from "./api";
 
 class LivenessApi extends Api {
 
-    public async checkLiveness3d(faceImage: Blob, zoomedFaceImage: Blob): Promise<boolean> {
+    public async checkLiveness3d(faceImage: Blob, zoomedFaceImage: Blob): Promise<{liveness: boolean, status: number}> {
         const formData = new FormData();
         formData.append('picture', faceImage);
         formData.append('zoomedPicture', zoomedFaceImage);
         const response = await this.call('v1/check_liveness_3d', formData);
-        return response.data.liveness;
+        return {liveness: response.data.liveness, status: response.data.status};
     }
 }
 
