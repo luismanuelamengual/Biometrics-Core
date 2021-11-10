5d5bc5e82ad36408d2d05380ff178a943b2df192
Luis Amengual <luismanuelamengual@gmail.com>
2021-11-10 06:48:50 -0300

    Subo version 1.2.1

--- src/api/liveness.ts
+++ src/api/liveness.ts
@@ -2,11 +2,11 @@ import {Api} from "./api";
 
 export class LivenessApi extends Api {
 
-    public async verifyLiveness(faceImage: Blob, zoomedFaceImage: Blob): Promise<{liveness: boolean, reason?: number}> {
+    public async verifyLiveness(faceImage: Blob, zoomedFaceImage: Blob): Promise<{liveness: boolean, status?: number}> {
         const formData = new FormData();
         formData.append('picture', faceImage);
         formData.append('zoomedPicture', zoomedFaceImage);
-        const response = await this.call('api/liveness/verify_liveness', formData);
+        const response = await this.call('api/verify_liveness', formData);
         return response.data;
     }
 }
